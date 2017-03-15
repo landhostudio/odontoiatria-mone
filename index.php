587032d244a79bba3d0a1920ee4809553da60473
h0r0man <roman.horokhovatskyy@gmail.com>
2017-03-15 17:48:31 +0100

    Added missing container to index page

--- index.php
+++ index.php
@@ -3,21 +3,23 @@
 <?php if (have_posts()): ?>
 
   <section class="index">
+    <div class="container">
 
-    <?php if (is_category()): ?>
-      <h2><?php single_cat_title(); ?></h2>
-    <?php elseif (is_tag()): ?>
-      <h2><?php single_tag_title(); ?></h2>
-    <?php elseif (is_search()): ?>
-      <h2><?php printf( __( 'Risultati di cerca per: %s', 'odontoiatria-mone' ), get_search_query() ); ?></h2>
-    <?php else: ?>
-      <h2><?php esc_html_e('Blog', 'odontoiatria-mone'); ?></h2>
-    <?php endif; ?>
-          
-    <?php while (have_posts()): the_post(); ?>
-      <?php get_template_part('template-parts/content', 'preview'); ?>
-    <?php endwhile; ?>
+      <?php if (is_category()): ?>
+        <h2><?php single_cat_title(); ?></h2>
+      <?php elseif (is_tag()): ?>
+        <h2><?php single_tag_title(); ?></h2>
+      <?php elseif (is_search()): ?>
+        <h2><?php printf( __( 'Risultati di cerca per: %s', 'odontoiatria-mone' ), get_search_query() ); ?></h2>
+      <?php else: ?>
+        <h2><?php esc_html_e('Blog', 'odontoiatria-mone'); ?></h2>
+      <?php endif; ?>
+            
+      <?php while (have_posts()): the_post(); ?>
+        <?php get_template_part('template-parts/content', 'preview'); ?>
+      <?php endwhile; ?>
 
+    </div>
   </section>
         
 <?php else: ?>
