5f1d1f6ac0c08515c8b0a7b7f68599a3d3a85c01
h0r0man <roman.horokhovatskyy@gmail.com>
2017-03-15 17:46:37 +0100

    Added container

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -1,4 +1,6 @@
 <article class="none">
-  <h2><?php esc_html_e('Nessun contenuto', 'odontoiatria-mone'); ?></h2>
-  <p><?php esc_html_e('Nessun contenuto rilevante che possa essere visualizzato.', 'odontoiatria-mone'); ?></p>
+  <div class="container">
+    <h2><?php esc_html_e('Nessun contenuto', 'odontoiatria-mone'); ?></h2>
+    <p><?php esc_html_e('Nessun contenuto rilevante che possa essere visualizzato.', 'odontoiatria-mone'); ?></p>
+  </div>
 </article>
