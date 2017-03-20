7f47d48e6f985e3331dd0be69b792c22263becd9
h0r0man <roman.horokhovatskyy@gmail.com>
2017-03-20 16:31:36 +0100

    Added date markup for preview articles

--- template-parts/content-preview.php
+++ template-parts/content-preview.php
@@ -1,6 +1,7 @@
 <article class="body__item body__item--article">
   <a rel="bookmark" href="<?php the_permalink(); ?>">
     <?php the_post_thumbnail(); ?>
+    <p class="date"><?php the_date(); ?></p>
     <h3><?php the_title(); ?></h3>
     <?php the_excerpt(); ?>
   </a>
