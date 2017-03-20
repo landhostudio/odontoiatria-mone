d0dd008304e9ef00954f1078b845a5b7372d0411
h0r0man <roman.horokhovatskyy@gmail.com>
2017-03-20 12:19:51 +0100

    Added footer markup

--- footer.php
+++ footer.php
@@ -1,6 +1,21 @@
     <footer role="contentinfo" class="footer">
       <div class="container">
-        <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> – <?php bloginfo('description'); ?>.</p>
+        <div class="footer__title">
+          <a rel="home" href="<?php echo esc_url(home_url('/')); ?>">
+            <span class="hidden"><?php bloginfo('name'); ?></span>
+            <img src="<?php bloginfo('template_url'); ?>/dist/img/logo-white.png" alt="" class="footer__info-logo">
+          </a>
+        </div>
+
+        <?php if (get_field('options_legal', options)): ?>
+          <div class="footer__legal">
+            <?php the_field('options_legal', options); ?>
+          </div>
+        <?php endif; ?>
+
+        <div class="footer__copy">
+          <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> – <?php bloginfo('description'); ?>.</p>
+        </div>
       </div>
     </footer>
 
