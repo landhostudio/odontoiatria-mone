7edc5167d0c4b94dd61684587809b322357d2be5
h0r0man <roman.horokhovatskyy@gmail.com>
2017-03-20 12:20:33 +0100

    Added theme AFC options

--- inc/acf.php
+++ inc/acf.php
@@ -1,5 +1,15 @@
 <?php
 
+if (function_exists('acf_add_options_page')) {
+
+  acf_add_options_page(array(
+		'page_title' 	=> 'Opzioni Tema',
+		'menu_title'	=> 'Opzioni',
+		'menu_slug' 	=> 'options-theme'
+	));
+  
+}
+
 function my_acf_google_map_api($api) {
   $api['key'] = 'AIzaSyBqcuWjVT_P93S8C9Vit5A2eMKAr8VfFbY';
   return $api;
